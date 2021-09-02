77fbcda4aab2b1a3d9040b7e42ccc12be83b927a
Vanakoff <andrey.vanakoff@gmail.com>
2021-09-03 00:57:41 +0200

    SettingsVC prototype created

--- AdvancedColorApp/SettingsViewController.swift
+++ AdvancedColorApp/SettingsViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    //MARK: Outlets
+    @IBOutlet var resultView: UIView!
+    @IBOutlet var doneButton: UIButton!
+    
+
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    //MARK: Settings
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        resultView.layer.cornerRadius = 10
+        doneButton.layer.cornerRadius = 5
+        
+        updateResultView()
     }
 
-
+        
+    //MARK: Actions
+    @IBAction func updateResultView() {
+        resultView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                             green: CGFloat(greenSlider.value),
+                                             blue: CGFloat(blueSlider.value),
+                                             alpha: 1)
+    }
 }
 
